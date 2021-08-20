25849fea3dcb31089577a256cf8310cb748cbec8
fedfon <fed.fonseca@gmail.com>
2021-08-20 00:08:34 -0400

    added MongoDb, updated .env (not visible) and implemented patient Schema

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 import express from "express";
 import bodyParser from "body-parser";
 const apis = require("./api/index");
+const mongoose = require("mongoose");
 
 //express server
 const app = express();
@@ -11,5 +12,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", apis);
+
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
