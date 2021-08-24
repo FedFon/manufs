b913381dc36d1b7d7a7f3238ea69c704aac72aa6
fedfon <fed.fonseca@gmail.com>
2021-08-24 13:47:50 -0400

    added form structure for when the patient is entered and submitted

--- frontend/src/components/pages/NewPatient.js
+++ frontend/src/components/pages/NewPatient.js
@@ -1,6 +1,7 @@
 import NewPatientForm from "../patients/NewPatientForm"
 
 const NewPatient = () => {
+
     return(
        <section>
            <h1>Adding a new patient</h1>
--- frontend/src/components/patients/NewPatientForm.js
+++ frontend/src/components/patients/NewPatientForm.js
@@ -1,6 +1,29 @@
-const NewPatientForm = () => {
+import { useRef } from "react";
+
+const NewPatientForm = (props) => {
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const phoneNumberRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredFirstName = firstNameRef.current.value;
+    const enteredLastName = lastNameRef.current.value;
+    const enteredEmail = emailRef.current.value;
+    const enteredPhoneNumber = phoneNumberRef.current.value;
+
+    const patientObject = {
+      firstName: enteredFirstName,
+      lastName: enteredLastName,
+      email: enteredEmail,
+      phone: enteredPhoneNumber,
+    };
+
+    props.onAddPatient(patientObject);
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div>
         <label htmlFor="firstName">First Name</label>
         <input type="text" />
