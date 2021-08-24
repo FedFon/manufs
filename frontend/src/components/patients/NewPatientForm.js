aaf576f566afec9ea779d70019700e58214fb66b
fedfon <fed.fonseca@gmail.com>
2021-08-23 20:20:44 -0400

    Added form for adding new patients

---
+++ frontend/src/components/patients/NewPatientForm.js
@@ -0,0 +1,27 @@
+const NewPatientForm = () => {
+  return (
+    <form>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="text" />
+      </div>
+      <div>
+        <button>Add Appointment</button>
+      </div>
+    </form>
+  );
+};
+
+export default NewPatientForm;
