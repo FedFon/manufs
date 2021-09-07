1571f816b6562e54d2bea15ef58fda985a11290d
fedfon <fed.fonseca@gmail.com>
2021-09-07 15:33:55 -0400

    fixed up the navbar with Bootstrap styling

--- frontend/src/components/layout/Navigation.js
+++ frontend/src/components/layout/Navigation.js
@@ -3,24 +3,56 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <header>
-      <div>
-        <i className="bi bi-heart-fill me-2 text-color: text-danger"></i>MANU
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/patient">Register a patient</Link>
-          </li>
-          <li>
-            <Link to="/list">Patient List</Link>
-          </li>
-          <li>
-            <Link to="/edit">Edit</Link>
-          </li>
-        </ul>
+      <div></div>
+      <nav className="navbar navbar-expand-xl bg-secondary navbar-dark">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav">
+            <li>
+              <i
+                className=" navbar-brand bi bi-eye me-2"
+                style={{ fontSize: "35px" }}
+              >
+                MANU
+              </i>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/patient">
+                Register a patient
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/list">
+                Patient List
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/edit">
+                Edit a Patient
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
 };
 
 export default Navigation;
+
+/**
+ * 
+ * 
+        
+        
+ */
