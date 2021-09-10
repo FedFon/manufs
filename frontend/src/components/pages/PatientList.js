ab070ee063374de667763e866a54eda478d2d556
fedfon <fed.fonseca@gmail.com>
2021-09-10 12:51:31 -0400

    Patient List now re-renders when a delete button is pressed

--- frontend/src/components/pages/PatientList.js
+++ frontend/src/components/pages/PatientList.js
@@ -13,51 +13,64 @@ const PatientList = (props) => {
   const patientListRef = useRef([]);
   //Setting state of loading for patient list
   const [isLoading, setIsLoading] = useState(true);
-
-  //This function will return an unordered list with Patients
-  const fillInPatients = () => {
-    return (
-      <ul>
-        {patientListRef.current.map((patient) => {
-          return (
-            <div key={patient.email}>
-              <Patient
-                key={patient._id}
-                firstName={patient.firstName}
-                lastName={patient.lastName}
-                email={patient.email}
-                phone={patient.phone}
-              />
-              <button
-                className="btn btn-secondary mb-4"
-                key={"delete " + patient._id}
-                onClick={() => {
-                  //if the delete button is clicked, the current patient object as well as its _id is passed onto the axios DELETE call
-                  axios.delete(`/patient/${patient._id}`, patient);
-                  console.log(patient);
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          );
-        })}
-      </ul>
-    );
-  };
+  const [deletePressed, setDeletePressed] = useState(false);
 
   //if the state changes the axios GET call will output the patient list
   useEffect(() => {
+    console.log("running");
     axios.get("/patient").then((res) => {
       patientListRef.current = res.data;
       setIsLoading(false);
     });
   }, []);
 
+  //if the delete button is pressed the state of deletePressed will change and this will execute
+  useEffect(() => {
+    if (deletePressed) {
+      console.log("running after press");
+      axios.get("/patient").then((res) => {
+        setIsLoading(true);
+        patientListRef.current = res.data;
+        setIsLoading(false);
+      });
+      setDeletePressed(false);
+    }
+  }, [deletePressed]);
+
   //either display loading... or the list when its ready
   return (
-    <ul>{isLoading ? <div>Loading...</div> : <div>{fillInPatients()}</div>}</ul>
+    <ul className="list-inline">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {patientListRef.current.map((patient) => {
+            return (
+              <div key={patient.email}>
+                <Patient
+                  key={patient._id}
+                  firstName={patient.firstName}
+                  lastName={patient.lastName}
+                  email={patient.email}
+                  phone={patient.phone}
+                />
+                <button
+                  className="btn btn-secondary mb-4"
+                  key={"delete " + patient._id}
+                  onClick={() => {
+                    setDeletePressed(true);
+                    //if the delete button is clicked, the current patient object as well as its _id is passed onto the axios DELETE call
+                    axios.delete(`/patient/${patient._id}`, patient);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </ul>
   );
 };
-
 export default PatientList;
