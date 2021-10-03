30a1543e398ea5117b1cf1fd850f816813e86003
fedfon <fed.fonseca@gmail.com>
2021-10-03 18:35:49 -0400

    Adding Dockerfile and .dockerignore files to frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 //express server
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 mongoose
   .connect(process.env.DB, {
