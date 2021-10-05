fea31538896e9b87cd984217ead32079e85adef4
Federico Fonseca <fed.fonseca@gmail.com>
2021-10-05 18:53:33 -0400

    Docker compose almost finished. Hot reload not working in VCS

--- frontend/src/components/patients/Patient.js
+++ frontend/src/components/patients/Patient.js
@@ -11,11 +11,11 @@ const Patient = (props) => {
         <div className="card-body">
           <div className="card-text">
             <dl className="row">
-              <dt className="col-sm-3 text-md-right">First name</dt>
+              <dt className="col-sm-3 text-md-right">First Name</dt>
               <dd className="col-sm-9">{props.firstName}</dd>
-              <dt className="col-sm-3 text-md-right">Last name</dt>
+              <dt className="col-sm-3 text-md-right">Last Name</dt>
               <dd className="col-sm-9">{props.lastName}</dd>
-              <dt className="col-sm-3 text-md-right">Email name</dt>
+              <dt className="col-sm-3 text-md-right">Email</dt>
               <dd className="col-sm-9">{props.email}</dd>
               <dt className="col-sm-3 text-md-right">Phone Number</dt>
               <dd className="col-sm-9">{props.phone}</dd>
